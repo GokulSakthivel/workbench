02576c0bb37ceac4e23de3057967867eeb89fce0
Gokul <gokulsakthivel001@gmail.com>
2018-01-04 21:07:05 +0530

    positive and negative c prog

--- posneg.c
+++ posneg.c
@@ -1,1 +1,15 @@
-https://github.com/GokulSakthivel/workbench.git
+#include <stdio.h>
+ 
+void main()
+{
+    int num;
+ 
+    printf("Enter a number:");
+    scanf("%d", &num);
+    if (num > 0)
+        printf("%d is a positive number \n", num);
+    else if (num < 0)
+        printf("%d is a negative number \n", num);
+    else
+        printf("Zero");
+}
