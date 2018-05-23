2ad8478b324c72da6690c4fc433d9d1f2d8efdfa
GokulSakthivel <gokulsakthivel001@gmail.com>
2018-05-23 20:25:20 +0530

    Update naturalsum.c

--- naturalsum.c
+++ naturalsum.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-	int sum=0,in;
+	int total=0,in;
 	printf("enter the number:");
 	scanf("%d",&in);
 	for(int i=1;i<=in;i++)
 	{
-	    sum=sum+i;
+	    total=total+i;
 	}
 	printf("\n %d",sum);
 	return 0;
