34a9101523d35503614e2d61dba2b725b5c6ead9
GokulSakthivel <gokulsakthivel001@gmail.com>
2018-06-17 12:09:10 +0530

    Create First5Mulitiple.java

---
+++ First5Mulitiple.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class First5Mulitiple{
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=1;i<=5;i++){
+		    System.out.print(i*n+" ");
+		}
+	}
+}
